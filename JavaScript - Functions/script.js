f5b3cd999a62892487f586dd6c5eaadb4c69037d
Ansh74284 <ansh15tiwari@gmail.com>
2023-12-15 18:49:27 +0530

    commit

---
+++ JavaScript - Functions/script.js
@@ -0,0 +1,17 @@
+//Jab aapka code aap turant nhi chalana chahte future mein chalana chahte ho 
+//jab aapka code aap reuse karna chahte ho 
+//jab aap code chalana chahte ho har baar with different data
+//functions ka matlab aap kuchh code ko likh kar koi naam de sakte ho and baad mein usey use kar sakte ho with that name as many times
+//functions = code ko naam dena 
+
+// function hellobolo(){
+//     console.log("Hello!");
+// }
+
+// hellobolo();
+
+function abcd(a, b, c){ //parameters = variables jinme value store hoti hai arguements waali.
+    console.log(a, b, c);
+}
+
+abcd(12, 13, 14); //arguements = real value jo hum dete h func chalate hue
