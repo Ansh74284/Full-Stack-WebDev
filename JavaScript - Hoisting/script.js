f9a101926bb04a744ffae80acf298f2dbf7011e3
Ansh74284 <ansh15tiwari@gmail.com>
2023-12-15 16:45:24 +0530

    commit

---
+++ JavaScript - Hoisting/script.js
@@ -0,0 +1,13 @@
+// var a; //declaration
+var b;
+//hosting and hoisting are different
+// hoisting - variable and functions are hoistedwhich means there declaration is moved on the top of code.
+
+// console.log(a);
+// a = 12; //Initalization
+
+console.log(b);
+b = 12;
+
+//Output will be undefined.
+
