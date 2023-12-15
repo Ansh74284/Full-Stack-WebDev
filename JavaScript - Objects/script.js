cb197feab846f82b8e54436e562588fd51537dd3
Ansh74284 <ansh15tiwari@gmail.com>
2023-12-15 20:42:32 +0530

    commit

---
+++ JavaScript - Objects/script.js
@@ -0,0 +1,26 @@
+// ek se zyada bande ki baat ki toh hua array, ek bande ke baare mein saari baat ki toh hua object.
+
+//object hai ek bande ki details ko hold karna, in a key value pair 
+
+//1) blank obj
+var a = {};
+
+//2) filled obj
+var a = {
+    age : 20,
+    name : "Ansh",
+    email : "email@male.com",
+    linkedin : "Ansh",
+    contact : 7428427167
+}
+
+var watch = {
+    brand : "Casio",
+    price : "16k",
+    color : "Black",
+    type : "Digital"
+    // kuchbolo: function(){},
+}
+
+// watch.type
+watch.brand = "Titan";
