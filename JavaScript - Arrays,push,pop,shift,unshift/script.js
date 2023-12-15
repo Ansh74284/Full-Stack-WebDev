1a474167c0e0afcd3783a49c24f6335ea3caec9d
Ansh74284 <ansh15tiwari@gmail.com>
2023-12-15 18:58:11 +0530

    commit

---
+++ JavaScript - Arrays,push,pop,shift,unshift/script.js
@@ -0,0 +1,11 @@
+//arrays = hum ek variable mein ek value store kar paate h par jab humein ek se zyada value store karni ho tab fir use hota h array ka, matlab ki array aapko freedom deta h ek se zyada variable use karne ka.
+
+// var a = [12, 13];
+//array = group of values;
+//push , pop , shift , unshift
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.pop();
+// arr.unshift(0);
+// arr.shift(0);
+// arr.splice(2,1);
