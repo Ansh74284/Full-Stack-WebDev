5fd576e2076effefb6ad3c46efe2971bf77ffd62
Ansh74284 <ansh15tiwari@gmail.com>
2023-12-15 18:38:22 +0530

    commit

---
+++ JavaScript - Loops/script.js
@@ -0,0 +1,22 @@
+//loops - for while
+//loop ka matlab hota h repeat
+// 1 1 1 1 1 1
+//1 2 3 4 5 6  repeatation of numberrs
+//3 baar Ansh Print karo
+
+// for(var i = 0; i<11; i++){ //0 - 10
+//     console.log(i);
+// }
+
+//25-50
+// for(var i=25; i<51; i++){
+//     console.log(i);
+// }
+
+//While Loop
+
+var a = 12;
+
+while(a<20){
+    a++;
+}
